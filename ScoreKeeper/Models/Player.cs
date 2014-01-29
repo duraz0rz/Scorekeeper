b2104247dbe01750783cfce0f02cf2fd9594f0c5
Marc Vertido <duracell@durashack.us>
2014-01-28 21:50:21 -0500

    Added MvvMLight thru NuGet. Added Player model and associated unit tests.

---
+++ ScoreKeeper/Models/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreKeeper
+{
+    public enum PlayerEvent
+    {
+        Add,
+        Subtract
+    }
+
+    public struct PlayerHistoryItem
+    {
+        public int Amount;
+        public PlayerEvent Type;
+    }
+
+    public class Player
+    {
+        public String Name { get; set; }
+        public int Score { get; private set; }
+        public Stack<PlayerHistoryItem> History { get; private set;}
+
+        public Player()
+        {
+            History = new Stack<PlayerHistoryItem>();
+        }
+
+        public void AddScore(int toAdd)
+        {
+            Score += toAdd;
+            History.Push(new PlayerHistoryItem { Amount = toAdd, Type = PlayerEvent.Add });
+        }
+
+        public void SubtractScore(int toSubtract)
+        {
+            Score -= toSubtract;
+            History.Push(new PlayerHistoryItem { Amount = toSubtract, Type = PlayerEvent.Subtract });
+        }
+    }
+}
---
+++ ScorekeeperTest/PlayerTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ScoreKeeper;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace ScorekeeperTest
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        Player player = null;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            player = new Player();
+        }
+
+        [TestMethod]
+        public void PlayerNameGetsSetCorrectly()
+        {
+            player.Name = "UNITTEST";
+            Assert.AreEqual<String>("UNITTEST", player.Name);
+        }
+
+        [TestMethod]
+        public void PlayerScoreGetsInitializedToZero()
+        {
+            Assert.AreEqual<int>(0, player.Score);
+        }
+
+        [TestMethod]
+        public void PlayerScoreGetsAddedCorrectly()
+        {
+            player.AddScore(20);
+            Assert.AreEqual<int>(20, player.Score);
+        }
+
+        [TestMethod]
+        public void PlayerScoreGetsSubtractedCorrectly()
+        {
+            player.SubtractScore(20);
+            Assert.AreEqual<int>(-20, player.Score);
+        }
+
+        [TestMethod]
+        public void PlayerHistoryGetsUpdatedWhenScoreIsAdded()
+        {
+            Assert.IsTrue(player.History.Count == 0, "History count is not zero!");
+            player.AddScore(20);
+            Assert.IsTrue(player.History.Count == 1, "History count is not one!");
+
+            PlayerHistoryItem currentHistory = player.History.Peek();
+
+            Assert.AreEqual<int>(20, currentHistory.Amount);
+            Assert.AreEqual<PlayerEvent>(PlayerEvent.Add, currentHistory.Type);
+        }
+
+        [TestMethod]
+        public void PlayerHistoryGetsUpdatedWhenScoreIsSubtracted()
+        {
+            Assert.IsTrue(player.History.Count == 0, "History count is not zero!");
+            player.SubtractScore(20);
+            Assert.IsTrue(player.History.Count == 1, "History count is not one!");
+
+            PlayerHistoryItem currentHistory = player.History.Peek();
+
+            Assert.AreEqual<int>(20, currentHistory.Amount);
+            Assert.AreEqual<PlayerEvent>(PlayerEvent.Subtract, currentHistory.Type);
+        }
+    }
+}
